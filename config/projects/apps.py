6f3b1539544e5ee854ffce5695240bf6b744c277
Saiful <mdsaifults08@gmail.com>
2026-01-17 22:32:41 +0530

    Fix AppConfig imports

--- config/projects/apps.py
+++ config/projects/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
-
 class ProjectsConfig(AppConfig):
-    name = 'projects'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'config.projects'
